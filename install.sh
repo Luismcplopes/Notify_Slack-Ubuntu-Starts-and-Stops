50631ebdfafbc1ce74c26ba79bf39b0ee3672896
luis lopes <luis.mcp.lopes@gmail.com>
2017-11-09 21:49:49 +0000

    ready tu use

--- install.sh
+++ install.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 #
-#
-# This script notifies slack
-# when this machine starts up
-# or shuts down
-#
-
 
 # donwload File notify_slack
 sudo wget https://git.io/vFBYU -O /etc/init.d/notify_slack
 sudo chmod u+x  /etc/init.d/notify_slack
+
 # donwload file slack_url
- sudo wget https://git.io/vFBY8 -O /etc/init.d/slack_url
+sudo wget https://git.io/vFBY8 -O /etc/init.d/slack_url
+
+# change this on slack_url file
+echo "Enter a slack webhooks : "
+read webhook
+echo "$webhook" >> /etc/init.d/slack_
 
 # change this on notify_slack file
-echo "Enter a slack webhooks : "
-read webhook
-# "channel":"#channel",
-echo "Enter a slack webhooks : "
-read webhook
-# "username" : "Server",
+echo "Enter a channel name like this #channel"
+read varchannel
+sed -i "s%<<channel>>%$varchannel%g" /etc/init.d/notify_slack
+echo "Enter a user name"
+read varusername
+sed -i "s%<<Server>>%$varusername%g" /etc/init.d/notify_slack
+echo "Enter a emoji name like this :ubuntu:"
+read varemoji
+sed -i "s%<<:ubuntu:>>%$varemoji%g" /etc/init.d/notify_slack
 
-
-echo "Enter a slack webhooks : "
-read webhook
-echo "$webhook" >> /etc/init.d/slack_url
-
-
-# test
+# start test
 sudo /etc/init.d/notify_slack start
-read -p "Press enter to continue"
-# test
+read -p "Please, confirm on slack for the start message"
+
+# stop test
 sudo /etc/init.d/notify_slack stop
-read -p "Press enter to continue"
-## Register the service
+read -p "Please, confirm on slack for the stop message"
+
+# Register the service
  sudo update-rc.d notify_slack default
- read -p "Press enter to continue"
+ read -p "Now you can do a reboot to test"
